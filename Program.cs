77e1b08b26d14e4247e9ecf8b9f67ba083d9ae08
4elovel <marcynakvova007@gmail.com>
2023-10-20 17:15:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    [DllImport("kernel32")]
+    private static extern bool Beep(int freq,int duration);
+    private static void Main(string[] args)
+    {
+        Random rand = new Random();
+        
+        for (int i = 0; i < 9; i++)
+        {
+            Beep(rand.Next(500, 1000), rand.Next(20, 20000));
+        }
+    }
+}
